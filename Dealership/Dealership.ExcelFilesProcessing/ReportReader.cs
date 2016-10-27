bdd19140c050ba47503232efcccc2fdbf8578ef6
Krassimir-ILLIEV <krassimir.illiev@mail.bg>
2016-10-28 01:28:17 +0300

    Unzipping files and reading reports

---
+++ Dealership/Dealership.ExcelFilesProcessing/ReportReader.cs
@@ -0,0 +1,31 @@
+﻿using Dealership.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dealership.ExcelFilesProcessing
+{
+    public class ReportReader
+    {
+        public void ParseExcelData(IEnumerable<DirectoryInfo> matchingDirectories)
+        {
+            //var salesReportFactory = new SalesReportGeneratorFromExcel(this.robotsFactoryData.Stores);
+            var excelSaleReportReader = new SalesReportsReaderExcel(Constants.ExcelSalesReportsConnectionStringPattern);
+
+            foreach (var dir in matchingDirectories)
+            {
+                foreach (var excelFile in dir.GetFiles(Constants.AllowedExcelFileExtensionPattern))
+                {
+                    var excelData = excelSaleReportReader.ReadReport(excelFile.FullName, dir.Name);
+                    //var salesReport = salesReportFactory.CreateSalesReport(excelData, dir.Name);
+                    //this.robotsFactoryData.SalesReports.Add(salesReport);
+                }
+            }
+
+            //this.robotsFactoryData.SaveChanges();
+        }
+    }
+}
---
+++ Dealership/Dealership.ExcelFilesProcessing/SalesReportsReaderExcel.cs
@@ -0,0 +1,93 @@
+﻿using DealerShip.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Dealership.ExcelFilesProcessing
+{
+    public class SalesReportsReaderExcel
+    {
+        private readonly string ConnectionString;
+
+        public SalesReportsReaderExcel(string connectonString)
+        {
+            this.ConnectionString = connectonString;
+        }
+        public ExcelSalesReport ReadReport(string reportPath, string reportDate)
+        {
+
+            ExcelSalesReport report = new ExcelSalesReport();
+            report.DateOfSale = DateTime.Parse(reportDate);
+            /* string LocalconnectionString = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                                 @"Data Source= {0};"+
+                                                 @"Extended Properties='Excel 12.0 Xml;HDR=YES;'"
+  , reportPath);*/
+            string LocalconnectionString = string.Format(this.ConnectionString, reportPath);
+            using (OleDbConnection connection = new OleDbConnection(LocalconnectionString)) //TODO: fix coupling, maybe use factory
+            {
+                connection.Open();
+                OleDbCommand excelCommand = new OleDbCommand("SELECT * FROM [Sales$]", connection);
+
+                using (var oleDbDataAdapter = new OleDbDataAdapter(excelCommand))
+                {
+                    var dataSet = new DataSet();
+                    oleDbDataAdapter.Fill(dataSet);
+
+                    using (var reader = dataSet.CreateDataReader())
+                    {
+                        report.DistributorName = this.ReadStoreName(reader);
+                        //report.Location = 
+                        reader.Read(); // Skip column names
+                        report.Records = this.GetReportEntries(reader);
+                    }
+                }
+                /*
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader["Name"];
+                        var score = reader["Score"];
+                        Console.WriteLine("Name - {0}; Score - {1}", name, score);
+
+                    }
+                }*/
+            }
+
+            return report;
+        }
+
+        private string ReadStoreName(DataTableReader reader)
+        {
+            if (reader.Read())
+            {
+                return reader[0].ToString();
+            }
+
+            return null;
+        }
+
+        private ICollection<ExcelSalesReportEntry> GetReportEntries(DataTableReader reader)
+        {
+            var reportEntries = new List<ExcelSalesReportEntry>();
+
+            while (reader.Read())
+            {
+                var isLastRow = string.IsNullOrEmpty(reader[1].ToString());
+
+                if (!isLastRow)
+                {
+                    reportEntries.Add(new ExcelSalesReportEntry()
+                    {
+                        ProductId = int.Parse(reader[0].ToString()),
+                        Quantity = int.Parse(reader[1].ToString()),
+                        UnitPrice = decimal.Parse(reader[2].ToString()),
+                    });
+                }
+            }
+
+            return reportEntries;
+        }
+    }
+}
