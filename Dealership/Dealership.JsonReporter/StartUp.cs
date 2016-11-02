9770cee1f57222d303c6eb96a74c80666c866a7e
pavelhristov <pavel.hristov_@abv.bg>
2016-11-02 15:42:49 +0200

    started JSON reports

---
+++ Dealership/Dealership.JsonReporter/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using Dealership.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dealership.JsonReporter
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new DealershipDbContext())
+            {
+                var employees = dbContext.Vehicles.ToList();
+                Directory.CreateDirectory("../../../Json-Reports");
+
+                foreach (var item in employees)
+                {
+                    string jsonObj = JsonConvert.SerializeObject(item, Formatting.Indented);
+
+                    Console.WriteLine(item.Brand.Name);
+
+                    //Console.WriteLine(item.FirstName + " " + item.LastName);
+                    //File.Create($"../../../Json-Reports/{item.Id}.json");
+                    //File.WriteAllText($"../../../Json-Reports/{item.Id}.json", jsonObj);
+                }
+            }
+        }
+    }
+}
