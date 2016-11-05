28ed125eb0661f3b26939c3787da8af9e73a7628
pavelhristov <pavel.hristov_@abv.bg>
2016-11-05 17:05:37 +0200

    MySQL reports added

--- Dealership/Dealership.JsonReporter/StartUp.cs
+++ Dealership/Dealership.JsonReporter/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using Dealership.Data;
+using Dealership.MySQL;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telerik.OpenAccess;
 
 namespace Dealership.JsonReporter
 {
@@ -13,6 +15,9 @@ namespace Dealership.JsonReporter
     {
         static void Main(string[] args)
         {
+            ICollection<JsonReport> jsonReports = new HashSet<JsonReport>();
+
+            // creating json reports.
             using (var dbContext = new DealershipDbContext())
             {
                 var vehicles = dbContext.Vehicles.ToList();
@@ -25,12 +30,13 @@ namespace Dealership.JsonReporter
 
                 foreach (var item in vehicles)
                 {
-                    var totalQuantitySold = sales.Where(x => x.VehicleId == item.Id).Sum(x => x.Quantity) ?? 0;
-                    var totalIncome = sales.Where(x => x.VehicleId == item.Id).Sum(x => x.Price*x.Quantity) ?? 0.00m;
+                    int reportId = item.Id;
+                    var totalQuantitySold = sales.Where(x => x.VehicleId == reportId).Sum(x => x.Quantity) ?? 0;
+                    var totalIncome = sales.Where(x => x.VehicleId == reportId).Sum(x => x.Price*x.Quantity) ?? 0.00m;
 
                     var jsonReportEntry = new JsonReportEntry()
                     {
-                        ProductId = item.Id,
+                        ProductId = reportId,
                         ProductName = item.Model,
                         ManufacturerName = item.Brand.Name,
                         TotalQuantitySold = totalQuantitySold,
@@ -38,9 +44,60 @@ namespace Dealership.JsonReporter
                     };
 
                     string jsonObj = JsonConvert.SerializeObject(jsonReportEntry, Formatting.Indented);
+                    var jsonReport = new JsonReport()
+                    {
+                        Id = reportId,
+                        JsonContent = jsonObj
+                    };
+                    jsonReports.Add(jsonReport);
 
-                    File.WriteAllText($"{directoryPath}/{item.Id}.json", jsonObj);
+                    File.WriteAllText($"{directoryPath}/{reportId}.json", jsonObj);
                 }
+                Console.WriteLine("JSON Reports created!");
+            }
+
+            // uploding reports to MySQL
+            UpdateDatabase();
+            using (var dbContext = new DaDbContext())
+            {
+                var dbreports = dbContext.GetAll<JsonReport>();
+                foreach (var item in jsonReports)
+                {
+                    if (dbreports.FirstOrDefault(x=>x.Id==item.Id) == null)
+                    {
+                        dbContext.Add(item);
+                    }
+                }
+                dbContext.SaveChanges();
+                Console.WriteLine("Reports Updated to MySQL database!");
+            }
+        }
+
+        private static void UpdateDatabase()
+        {
+            using (var context = new DaDbContext())
+            {
+                var schemaHandler = context.GetSchemaHandler();
+                EnsureDB(schemaHandler);
+            }
+        }
+
+        private static void EnsureDB(ISchemaHandler schemaHandler)
+        {
+            string script = null;
+            if (schemaHandler.DatabaseExists())
+            {
+                script = schemaHandler.CreateUpdateDDLScript(null);
+            }
+            else
+            {
+                schemaHandler.CreateDatabase();
+                script = schemaHandler.CreateDDLScript();
+            }
+
+            if (!string.IsNullOrEmpty(script))
+            {
+                schemaHandler.ExecuteDDLScript(script);
             }
         }
     }
---
+++ Dealership/Dealership.MySQL/FluentModelMetadataSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.OpenAccess.Metadata.Fluent;
+
+namespace Dealership.MySQL
+{
+    public partial class FluentModelMetadataSource : FluentMetadataSource
+    {
+        protected override IList<MappingConfiguration> PrepareMapping()
+        {
+            List<MappingConfiguration> configurations =
+                new List<MappingConfiguration>();
+
+            var jsonReportMapping = new MappingConfiguration<JsonReport>();
+            jsonReportMapping.MapType(jsonReport => new
+            {
+                Id = jsonReport.Id,
+                JsonConent = jsonReport.JsonContent
+            }).ToTable("JsonReport");
+            jsonReportMapping.HasProperty(j => j.Id).IsIdentity();
+
+            configurations.Add(jsonReportMapping);
+
+            return configurations;
+        }
+    }
+}
