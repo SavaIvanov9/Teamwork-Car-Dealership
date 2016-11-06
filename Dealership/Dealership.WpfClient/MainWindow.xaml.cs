93eba2a467df081a0f2380c6d6de4b3c3f7fbc64
Georgi Velikov <gmvelikov@gmail.com>
2016-11-06 12:09:14 +0200

    wpf started

---
+++ Dealership/Dealership.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+
+using Dealership.Common;
+using Dealership.Data;
+using Dealership.Data.Seeders;
+using Dealership.DataSeed.Seeders;
+using Dealership.ExcelFilesProcessing;
+using Dealership.MongoDb;
+using Dealership.XmlFilesProcessing.Readers;
+
+namespace Dealership.WpfClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnLoadSqlClick(object sender, RoutedEventArgs e)
+        {
+            this.SeedDataFromMongo();
+
+            this.SeedDataFromXml();
+
+            this.SeedDataFromSalesReports();
+        }
+
+        private void SeedDataFromMongo()
+        {
+            string mongoDbConnectionString = Constants.MongoDbConnectionStringLocal;
+            string mongoDbDatabaseName = Constants.MongoDbDatabaseNameLocal;
+
+            var mongoDbHandler = new MongoDbHandler(mongoDbConnectionString, mongoDbDatabaseName);
+            var dealershipDbContext = new DealershipDbContext();
+            
+            if (!mongoDbHandler.IsDataSeeded(dealershipDbContext))
+            {
+                mongoDbHandler.SeedData(dealershipDbContext);
+            }
+
+            MessageBox.Show("Mongo Success");
+        }
+
+        private void SeedDataFromXml()
+        {
+            var xmlEmployeeReader = new XmlEmployeeReader();
+
+            var dbContext = new DealershipDbContext();
+            var data = new DealershipData(dbContext);
+            var employeeSeeder = new EmployeeSeeder(data);
+
+            var employeeSeedUtil = new EmployeeSeedUtil(xmlEmployeeReader, employeeSeeder);
+
+            employeeSeedUtil.Seed();
+
+            MessageBox.Show("XML Success");
+        }
+
+        private void SeedDataFromSalesReports()
+        {
+            SeedingSQLDBFromZip s = new SeedingSQLDBFromZip();
+
+            var processor = new ZipUnpacker();
+            processor.Unpack(Constants.PathToZipFile, Constants.PathToUnzip);
+
+            var matchingDirectories = Utility.GetDirectoriesByPattern(Constants.PathToUnzippedFiles);
+            ReportReader reportReader = new ReportReader();
+            reportReader.ParseExcelData(matchingDirectories);
+
+            MessageBox.Show("Excell Success");
+        }
+    }
+}
