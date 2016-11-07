0daef767706673a4a921fcc2da2345055c0b17f5
Galvaneyes <m.v.manchev@gmail.com>
2016-11-07 20:45:17 +0200

    Fixed pdf look

--- Dealership/Dealership.XmlFilesProcessing/Writers/Common/PdfAggregatedDailySalesReportWriter.cs
+++ Dealership/Dealership.XmlFilesProcessing/Writers/Common/PdfAggregatedDailySalesReportWriter.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Text;
 using Dealership.Reports.Models.Contracts;
 using Dealership.XmlFilesProcessing.Writers.Contracts;
 using iTextSharp.text;
@@ -39,27 +41,45 @@ namespace Dealership.XmlFilesProcessing.Writers.Common
 
                 const int cols = 5;
                 PdfPTable table = new PdfPTable(cols);
+                Font font = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+                Chunk brand = new Chunk("Brand", font);
+                Chunk model = new Chunk("Model", font);
+                Chunk quantity = new Chunk("Quantity", font);
+                Chunk unitPrice = new Chunk("Unit Price", font);
+                Chunk totalPrice = new Chunk("Total Price", font);
+
+                PdfPCell brandCell = new PdfPCell(new Phrase(brand));
+                PdfPCell modelCell = new PdfPCell(new Phrase(model));
+                PdfPCell quantityCell = new PdfPCell(new Phrase(quantity));
+                PdfPCell unitPriceCell = new PdfPCell(new Phrase(unitPrice));
+                PdfPCell totalPriceCell = new PdfPCell(new Phrase(totalPrice));
 
                 foreach (var dailyReport in Report)
                 {
-                    PdfPCell day = new PdfPCell(new Phrase(dailyReport.Date.ToString()));
+                    PdfPCell day = new PdfPCell(new Phrase(new Chunk($"{dailyReport.Date:dd-MMM-yyyy}", font)));
                     day.Colspan = cols;
                     day.HorizontalAlignment = 0; //0=Left, 1=Centre, 2=Right
                     table.AddCell(day);
+  
+                    table.AddCell(brandCell);
+                    table.AddCell(modelCell);
+                    table.AddCell(quantityCell);
+                    table.AddCell(unitPriceCell);
+                    table.AddCell(totalPriceCell);
 
                     foreach (var entity in dailyReport.DailyEntities)
                     {
                         table.AddCell(entity.Brand);
-                        table.AddCell(entity.Model);
+                        table.AddCell(entity.Model);                                           
                         table.AddCell(entity.Quantity.ToString());
                         table.AddCell($"{entity.UnitPrice:F2}");
                         table.AddCell($"{entity.TotalPrice:F2}");
                     }
 
-                    PdfPCell totalSum = new PdfPCell(new Phrase(dailyReport.TotalDailySales.ToString()));
-                    day.Colspan = cols;
-                    day.HorizontalAlignment = 2; //0=Left, 1=Centre, 2=Right
-                    table.AddCell(day);
+                    PdfPCell totalSum = new PdfPCell(new Phrase($"Total daily sales: {dailyReport.TotalDailySales:F2}"));
+                    totalSum.Colspan = cols;
+                    totalSum.HorizontalAlignment = 2; //0=Left, 1=Centre, 2=Right
+                    table.AddCell(totalSum);
                 }
 
                 document.Add(table);
