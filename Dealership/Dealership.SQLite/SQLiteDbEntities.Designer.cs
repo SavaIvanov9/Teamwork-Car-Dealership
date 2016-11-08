37dd0258f290cd28e0efa5102d46173ef9332d0d
Georgi Velikov <gmvelikov@gmail.com>
2016-11-08 23:59:07 +0200

    wpf dont work

--- Dealership/Dealership.SQLite/SQLiteDbEntities.Designer.cs
+++ Dealership/Dealership.SQLite/SQLiteDbEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\GitHub\Team works\Teamwork-Car-Dealership\Dealership\Dealership.SQLite\SQLiteDbEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\gvelikov\Desktop\Teamwork\Teamwork-Car-Dealership\Dealership\Dealership.SQLite\SQLiteDbEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Dealership/Dealership.WpfClient/MainWindow.xaml.cs
+++ Dealership/Dealership.WpfClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 
 using Dealership.Common;
@@ -6,11 +7,18 @@ using Dealership.Data;
 using Dealership.Data.Seeders;
 using Dealership.DataSeed.Seeders;
 using Dealership.ExcelFilesProcessing;
+using Dealership.ExcelReportGenerator;
+using Dealership.ExcelReportGenerator.Contracts;
+using Dealership.JsonReporter;
 using Dealership.MongoDb;
 using Dealership.XmlFilesProcessing.Readers;
 using Dealership.Models.Models.XmlSource;
 using Dealership.Models.Models.SalesReportSource;
 using Dealership.Models.Models.MongoDbSource;
+using Dealership.Reports.Models;
+using Dealership.Reports.Models.Contracts;
+using Dealership.XmlFilesProcessing.Writers.Common;
+using Dealership.XmlFilesProcessing.Writers.Contracts;
 
 namespace Dealership.WpfClient
 {
@@ -32,11 +40,40 @@ namespace Dealership.WpfClient
             MessageBox.Show("Data successfully loaded into SQL!");
 
             this.BtnXmlReports.Visibility = Visibility.Visible;
+            this.BtnPdfReports.Visibility = Visibility.Visible;
+            this.BtnJsonReports.Visibility = Visibility.Visible;
         }
 
         private void BtnXmlReportsClick(object sender, RoutedEventArgs e)
         {
-            //Generate XML Reports
+            this.GenerateXmlShopReport();
+
+            this.GenerateXmlDailyShopReport();
+
+            MessageBox.Show("Xml reports generated successfully!");
+        }
+
+        private void BtnPdfReportsClick(object sender, RoutedEventArgs e)
+        {
+            this.GeneratePdfAggregateDailySalesReport();
+
+            MessageBox.Show("Pdf reports generated successfully!");
+        }
+
+        private void BtnJsonReportsClick(object sender, RoutedEventArgs e)
+        {
+            this.GenerateJsonReports();
+
+            MessageBox.Show("Json reports generated successfully!");
+
+            this.BtnExcellReports.Visibility = Visibility.Visible;
+        }
+
+        private void BtnExcellReportsClick(object sender, RoutedEventArgs e)
+        {
+            this.GenerateExcelReport();
+
+            MessageBox.Show("Excell reports generated successfully!");
         }
 
         private void SeedDataFromMongo()
@@ -96,7 +133,6 @@ namespace Dealership.WpfClient
             // MessageBox.Show("Data from XML loaded successfully!");
         }
 
-
         private void SeedDataFromSalesReports()
         {
             using (var dbContext = new DealershipDbContext())
@@ -118,6 +154,59 @@ namespace Dealership.WpfClient
 
             // MessageBox.Show("Data from Excel loaded successfully!");
             }
+        }
+
+        private void GenerateExcelReport()
+        {
+            IExcelReportGenerator excelReportGenerator = new ReportGenerator();
+
+            string reportsPath = Constants.ExcelReportsPath;
+            string excelReportName = Constants.ExcelReportName;
+
+            excelReportGenerator.GenerateExcelReport(reportsPath, excelReportName);
+        }
+
+        public void GenerateJsonReports()
+        {
+            string directoryPath = Constants.JsonReportsPath;
+
+            var reports = JsonReports.GenerateReports(directoryPath);
+
+            JsonReports.SeedReportsToMySQL(reports);
+        }
+
+        public void GenerateXmlDailyShopReport()
+        {
+
+            var dbContext = new DealershipDbContext();
+            ReportQuery query = new ReportQuery();
+
+            ICollection<IXmlDailyShopReport> dailyReport = new List<IXmlDailyShopReport>();
+            IReportWriter dailyWrite = new XmlDailyShopReportWriter(query.DailyShopReport(dbContext, dailyReport));
+
+            dailyWrite.Write();
+        }
+
+        public void GenerateXmlShopReport()
+        {
+            var dbContext = new DealershipDbContext();
+            ReportQuery query = new ReportQuery();
+
+            ICollection<IXmlShopReport> totalReport = new List<IXmlShopReport>();
+            IReportWriter totalWriter = new XmlShopReportWriter(query.ShopReport(dbContext, totalReport));
+
+            totalWriter.Write();
+        }
+
+        public void GeneratePdfAggregateDailySalesReport()
+        {
+            var dbContext = new DealershipDbContext();
+            ReportQuery query = new ReportQuery();
+
+            ICollection<IPdfAggregatedDailySalesReport> totalReport = new List<IPdfAggregatedDailySalesReport>();
+            IReportWriter totalWriter = new PdfAggregatedDailySalesReportWriter(query.AggregatedDailySalesReports(dbContext, totalReport));
+
+            totalWriter.Write();
         }
     }
 }
